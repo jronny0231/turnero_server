55855782e080f7e2aede7ceaa24c42b8eb38c2de
JRonny Laptop-Nikii <jr.martinezvaldez@gmail.com>
2023-09-10 17:15:32 -0400

    Deleting dist folder and fix start command line

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,6 @@ import { initialize } from "./core/admin";
 initialize() // Initialize constants cache core data
 
 // Page not found: must be after all routes
-App.use('/*', (req: Request, res: Response) => {
+App.use((req: Request, res: Response) => {
     return res.status(404).json({message: `The resource on ${req.originalUrl} could not be found!`});
 })
