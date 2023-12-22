cadf38a4515cd3419b62ce99d2994589e3d191fb
PC INVIS <jr.martinezvaldez@gmail.com>
2023-12-22 18:00:17 -0400

    add schema based agent and deps types

--- src/@types/agent.d.ts
+++ src/@types/agent.d.ts
@@ -1,4 +1,4 @@
-import { Agentes, Clientes } from '@prisma/client'
+import { Agentes, Clientes, Departamentos, Departamentos_sucursales, Sucursales, Tipos_agentes } from '@prisma/client'
 
 export interface AgentWithUser extends Agentes {
     usuario: {
@@ -7,5 +7,23 @@ export interface AgentWithUser extends Agentes {
         username: string
         password: string
         rol_id: number
+    }
+}
+
+export interface AuthedAgent extends Agentes {
+    tipo_agente: Tipos_agentes
+    departamento_sucursal: DepartmentOffice
+}
+
+export interface DepartmentOffice extends Pick<Departamentos_sucursales, 'estatus'> {
+    departamento: Pick<Departamentos, 'id' | 'descripcion'>
+    sucursal: Pick<Sucursales, 'id' | 'descripcion'>
+}
+
+export interface SuperAgent extends Pick<Agentes, 'nombre' | 'id'> {
+    tipo_agente: Pick<Tipos_agentes, 'nombre'>
+    departamento_sucursales: {
+        departamento: Pick<Departamentos, 'descripcion'>
+        sucursal: Pick<Sucursales, 'descripcion'>
     }
 }
